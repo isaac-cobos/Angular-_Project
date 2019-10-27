edfe117a870c96c152006c8b5ec2399ed873fb82
isaac-enrique.cobos <isaac-enrique.cobos@soprasteria.com>
2019-10-27 16:05:40 +0100

    Section 8 finalizado

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -22,6 +22,8 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
 // temporal
 import { IncrementarComponent } from '../components/incrementar/incrementar.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 
@@ -33,7 +35,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementarComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -4,18 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SidebarService {
-
   menu: any = [
     {
       titulo: 'Principal',
       icono: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Dashboard', url: '/dashboard' },
-        { titulo: 'ProgressBar', url: '/progress'},
-        { titulo: 'Gráficas', url: '/graficas1'}
+        { titulo: 'ProgressBar', url: '/progress' },
+        { titulo: 'Gráficas', url: '/graficas1' },
+        { titulo: 'Promesas', url: '/promesas' },
+        { titulo: 'Rxjs', url: '/rxjs' }
       ]
     }
-  ]
+  ];
 
-  constructor() { }
+  constructor() {}
 }
